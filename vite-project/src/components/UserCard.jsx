b993c29cca39a72927d94a2c73a86113c9b6fdf4
Vinicio Daniele <viniciodanielem@gmail.com>
2026-03-08 13:45:01 +0000

    setState

--- vite-project/src/components/UserCard.jsx
+++ vite-project/src/components/UserCard.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 
 const UserCard = (props) => {
     const { age, email } = props;
-    const [name, setName] = useState("Vini Daniele"); // ✅ top level of the component
+    const [name, setName] = useState("Vini Daniele");
+    const resetName = () => {
+        setName("");
+    } 
 
     return (
         <div>
             <h3>Hello, {name}!</h3>
             <p>Age: {age} Email: {email}</p>
+            <button onClick={resetName}>Reset Name</button>
         </div>
     );
 }
