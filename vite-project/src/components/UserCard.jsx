47afefd7044c8c5f0b4ccd2ae4c8d5ab38f2eacf
Vinicio Daniele <viniciodanielem@gmail.com>
2026-03-08 14:10:31 +0000

    conditional

--- vite-project/src/components/UserCard.jsx
+++ vite-project/src/components/UserCard.jsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 
 const UserCard = (props) => {
     const { age, email } = props;
-    const [name, setName] = useState("Vini Daniele");
-    const resetName = () => {
-        setName("");
-    } 
+    const [name, setName,] = useState("Vini Daniele")
+    const [showingName, showName] = useState(true);
+    const resetName = () => { setName("");}
+
+    const showNametoggle = () => {
+        showName(!showingName);
+
+    };
+
+
 
     return (
         <div>
             <h3>Hello, {name}!</h3>
             <p>Age: {age} Email: {email}</p>
+            <button onClick={showNametoggle}>{showingName ? "Hide Name" : "Show Name"}</button>
+            <h4>{showingName ? name : null}</h4>
             <button onClick={resetName}>Reset Name</button>
         </div>
     );
